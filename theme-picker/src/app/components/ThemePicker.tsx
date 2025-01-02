bb033333197605bb84a546d72794a98485d0cb7c
Jan Burzinski <156842394+janburzinski@users.noreply.github.com>
2025-01-02 09:29:43 +0100

    nextjs init

---
+++ theme-picker/src/app/components/ThemePicker.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+export const ThemePicker = ({}) => {
+  return (
+    <>
+      <Image
+        className="dark:invert"
+        src="/next.svg"
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />
+      <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li className="mb-2">
+          Get started by editing{" "}
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            src/app/page.tsx
+          </code>
+          .
+        </li>
+      </ol>
+
+      <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <a
+          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="dark:invert"
+            src="/vercel.svg"
+            alt="Vercel logomark"
+            width={20}
+            height={20}
+          />
+          pick random theme
+        </a>
+      </div>
+    </>
+  );
+};
